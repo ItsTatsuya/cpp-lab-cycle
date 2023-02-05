10b4f4d0522a53b795b75e2a3520766db9ada3f9
Tatsuya <pruthvikanth007@gmail.com>
2023-02-05 18:51:47 +0530

    question 5 done

---
+++ Lab Cycle 1/Question 5/code.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class Matrix
+{
+    int **p;
+    int row, column;
+
+public:
+    Matrix(int x, int y)
+    {
+        row = x;
+        column = y;
+        p = new int *[row];
+        for (int i = 0; i < row; ++i)
+        {
+            p[i] = new int[column];
+        }
+    }
+    void getdata();
+    void putdata();
+    ~Matrix()
+    {
+        cout << "\nMemory freed up!\n";
+    }
+};
+
+void Matrix::getdata()
+{
+    cout << "\nEnter the Values\n";
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < column; ++j)
+        {
+            cout << "Row "<<i+1<<" | "<<"Column "<<j+1<<" : " ;
+            cin >> p[i][j];
+        }
+    }
+}
+
+void Matrix::putdata()
+{
+    cout << endl;
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < column; ++j)
+        {
+            cout << p[i][j] << "  ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int x, y;
+    cout << "Enter the rows of matrix: ";
+    cin >> x;
+    cout << "Enter the column of matrix: ";
+    cin >> y;
+    Matrix Object(x, y);
+    Object.getdata();
+    Object.putdata();
+    return 0;
+}
