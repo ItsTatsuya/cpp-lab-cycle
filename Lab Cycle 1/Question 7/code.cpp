30483a56c14655699c38c4f62c625d9be1151793
tatsuya <pruthvikanth007@gmail.com>
2023-02-07 15:14:47 +0530

    yes

---
+++ Lab Cycle 1/Question 7/code.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Matrix
+{
+    int **p;
+    int row, column;
+
+public:
+    Matrix(int x, int y);
+    void getdata();
+    void matrix_add();
+    void matrix_mult();
+    void matrix_transpose();
+    void matrix_trace();
+
+
+}
