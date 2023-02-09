2690990803ab6006fa129ae0f1a2b0801988664f
Tatsuya <pruthvikanth007@gmail.com>
2023-02-09 19:34:07 +0530

    question 9 done

---
+++ Lab Cycle 1/Question 9/README.md
@@ -0,0 +1,6 @@
+Question 9
+=======
+
+Write a C++ program to design a class having static member function named
+showcount() which has the property of displaying the number of objects created
+of the class.
--- Lab Cycle 1/Question 9/code.cpp
+++ Lab Cycle 1/Question 9/code.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 
-class Static{
+class Static
+{
     static int count;
+
 public:
     Static();
-    static void showcount();
-
+    void static showcount();
 };
 
-int Static::count = 0;
+int Static::count;
 
-Static::Static(){
+Static::Static()
+{
     count++;
 }
 
-void Static::showcount(){
-    std::cout<<"Number of Objects Created: "
+void Static::showcount()
+{
+    std::cout << "\nNumber of Objects Created: "<<count;
+}
+
+int main(){
+    int x;
+    std::cout<<"Enter the number of objects to create: ";
+    std::cin>>x;
+    Static st[x];
+    st[x].showcount();
+    return 0;
 }
