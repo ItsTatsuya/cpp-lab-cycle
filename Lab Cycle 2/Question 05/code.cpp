a585c01889966bb136fc181a755f74d512a14574
Tatsuya <87627258+ItsTatsuya@users.noreply.github.com>
2023-05-01 14:27:25 +0530

    qnfix

--- Lab Cycle 2/Question 05/code.cpp
+++ Lab Cycle 2/Question 05/code.cpp
@@ -1,57 +1,91 @@
-#include<iostream>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-class A
+class student
 {
-    int a;
+    protected:
+        int roll_no;
+        char name[20];
     public:
-    A(int x)
-    {
-        a=x;
-    }
-    void display()
-    {
-        cout<<"Value of a is "<<a<<endl;
-    }
+        student(int r, char n[])
+        {
+            roll_no = r;
+            strcpy(name, n);
+        }
+        void display()
+        {
+            cout<<"Roll No: "<<roll_no<<endl;
+            cout<<"Name: "<<name<<endl;
+        }
 };
 
-class B:public A
+class marks: public student
 {
-    int b;
+    protected:
+        int m1, m2, m3;
     public:
-    B(int x,int y):A(x)
-    {
-        b=y;
-    }
-    void display()
-    {
-        cout<<"Value of b is "<<b<<endl;
-    }
+        marks(int r, char n[], int m1, int m2, int m3): student(r, n)
+        {
+            this->m1 = m1;
+            this->m2 = m2;
+            this->m3 = m3;
+        }
+        void display()
+        {
+            student::display();
+            cout<<"Marks 1: "<<m1<<endl;
+            cout<<"Marks 2: "<<m2<<endl;
+            cout<<"Marks 3: "<<m3<<endl;
+        }
 };
 
-class C:public B
+class result: public marks
 {
-    int c;
+    int total;
+    float avg;
     public:
-    C(int x,int y,int z):B(x,y)
-    {
-        c=z;
-    }
-    void display()
-    {
-        cout<<"Value of c is "<<c<<endl;
-    }
+        result(int r, char n[], int m1, int m2, int m3): marks(r, n, m1, m2, m3)
+        {
+            total = m1 + m2 + m3;
+            avg = total / 3.0;
+        }
+        void display()
+        {
+            marks::display();
+            cout<<"Total: "<<total<<endl;
+            cout<<"Average: "<<avg<<endl;
+        }
 };
 
 int main()
-{
-    cout<<"Enter three numbers: ";
-    int a,b,c;
-    cin>>a>>b>>c;
-    C obj(a,b,c);
-    obj.A::display();
-    obj.B::display();
-    obj.display();
+{   
+    int n, r, m1, m2, m3;
+    cout<<"Enter the number of students: ";
+    cin>>n;
+    result *s[n];
+    for(int i = 0; i < n; i++)
+    {   
+        cout<<"Student "<<i + 1<<endl;
+        cout<<"Enter the roll no: ";
+        cin>>r;
+        cout<<"Enter the name: ";
+        char n[20];
+        cin>>n;
+        cout<<"Enter the marks 1: ";
+        cin>>m1;
+        cout<<"Enter the marks 2: ";
+        cin>>m2;
+        cout<<"Enter the marks 3: ";
+        cin>>m3;
+        s[i] = new result(r, n, m1, m2, m3);
+        cout<<endl;
+    }
+    for(int i = 0; i < n; i++)
+    {
+        cout<<"Student "<<i + 1<<endl;
+        s[i]->display();
+        cout << endl;
+    }
     return 0;
 }
-
